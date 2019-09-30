b47427531cd5da0122a0d027f19375553f84cb1b
Promeathean0056 <34377463+Promeathean0056@users.noreply.github.com>
2019-09-29 22:16:23 -0400

    Add files via upload

---
+++ Lab 5/enrollmentApplication/Models/Enrollment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace enrollmentApplication.Models
+{
+    public class Enrollment
+    {
+        int Enrollment_id;
+        int Student_id;
+        int Course_id;
+        Char Grade;
+        Object Student;
+        Object Course;
+        bool IsActive;
+        String AssignedCampus;
+        String EnrollmentSemester;
+        int EnrollmentYear;
+
+    }
+}
